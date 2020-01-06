bce9a4daaf05f4edd0d55e6cb160199df5e998f6
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2020-01-05 21:05:34 -0800

    Add tests for Campaigns.Members

--- campaign_test.go
+++ campaign_test.go
@@ -9,8 +9,9 @@ import (
 )
 
 const (
-	testCampaignIndex string = "test_data/campaign_index.json"
-	testCampaignGet   string = "test_data/campaign_get.json"
+	testCampaignIndex   string = "test_data/campaign_index.json"
+	testCampaignGet     string = "test_data/campaign_get.json"
+	testCampaignMembers string = "test_data/campaign_members.json"
 )
 
 func TestCampaignService_Index(t *testing.T) {
@@ -109,7 +110,7 @@ func TestCampaignService_Get(t *testing.T) {
 					User: User{
 						ID:     222,
 						Name:   "Henry",
-						Avatar: "users/aFstunGZFZ53yr3Zy19IqKBoceuwFGaAJVFfOKUf.png",
+						Avatar: "some_avatar.png",
 					},
 				},
 			},
@@ -195,6 +196,122 @@ func TestCampaignService_Get(t *testing.T) {
 			c, _ := testClient(test.status, f)
 
 			got, err := c.Campaigns.Get(test.args.campID)
+			if (err != nil) != test.wantErr {
+				t.Fatalf("got: <%v>, want error: <%v>", err, test.wantErr)
+			}
+			if diff := cmp.Diff(got, test.want); diff != "" {
+				t.Errorf("mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestCampaignService_Members(t *testing.T) {
+	mems := []*Member{
+		&Member{
+			ID: 111,
+			User: User{
+				ID:     222,
+				Name:   "Jon",
+				Avatar: "jon_brooding.png",
+			},
+		},
+		&Member{
+			ID: 333,
+			User: User{
+				ID:     444,
+				Name:   "Daenerys",
+				Avatar: "daeny_burning_something.png",
+			},
+		},
+		&Member{
+			ID: 555,
+			User: User{
+				ID:     666,
+				Name:   "Stannis",
+				Avatar: "stannis_also_brooding.png",
+			},
+		},
+	}
+	type args struct {
+		campID int
+	}
+	tests := []struct {
+		name    string
+		status  int
+		file    string
+		args    args
+		want    []*Member
+		wantErr bool
+	}{
+		{
+			name:    "StatusOK, valid response, valid args",
+			status:  http.StatusOK,
+			file:    testCampaignMembers,
+			args:    args{campID: 5272},
+			want:    mems,
+			wantErr: false,
+		},
+		{
+			name:    "Status OK, valid response, invalid args",
+			status:  http.StatusOK,
+			file:    testCampaignMembers,
+			args:    args{campID: -123},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "Status OK, empty response, valid args",
+			status:  http.StatusOK,
+			file:    testFileEmpty,
+			args:    args{campID: 5272},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "Status OK, empty response, invalid args",
+			status:  http.StatusOK,
+			file:    testFileEmpty,
+			args:    args{campID: -123},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "StatusUnauthorized, valid args",
+			status:  http.StatusUnauthorized,
+			file:    testFileEmpty,
+			args:    args{campID: 5272},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "StatusForbidden, valid args",
+			status:  http.StatusForbidden,
+			file:    testFileEmpty,
+			args:    args{campID: 5272},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "StatusNotFound, valid args",
+			status:  http.StatusNotFound,
+			file:    testFileEmpty,
+			args:    args{campID: 5272},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			f, err := os.Open(test.file)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer f.Close()
+
+			c, _ := testClient(test.status, f)
+
+			got, err := c.Campaigns.Members(test.args.campID)
 			if (err != nil) != test.wantErr {
 				t.Fatalf("got: <%v>, want error: <%v>", err, test.wantErr)
 			}
