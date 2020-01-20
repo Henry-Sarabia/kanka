b61e18f344af83a6b0d1b18ab17a793b8b768c65
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2020-01-19 20:40:12 -0800

    Improve AttributeService's function readability

--- attribute.go
+++ attribute.go
@@ -53,18 +53,20 @@ type Attributes struct {
 // AttributeService handles communication with the Attribute endpoint.
 type AttributeService service
 
-// Index returns the list of all Attributes in the Campaign associated with campID.
+// Index returns the list of all Attributes for the entity associated with
+// entID in the Campaign associated with campID.
 // If a non-nil time is provided, Index will only return Attributes that have
 // been changed since that time.
 func (as *AttributeService) Index(campID int, entID int, sync *time.Time) ([]*Attribute, error) {
-	end, err := EndpointCampaign.ID(campID)
-	if err != nil {
+	var err error
+	end := EndpointCampaign
+
+	if end, err = end.ID(campID); err != nil {
 		return nil, fmt.Errorf("invalid Campaign ID: %w", err)
 	}
 	end = end.Concat(endpointEntity)
 
-	end, err = end.ID(entID)
-	if err != nil {
+	if end, err = end.ID(entID); err != nil {
 		return nil, fmt.Errorf("invalid Entity ID: %w", err)
 	}
 	end = end.Concat(as.end)
@@ -77,31 +79,30 @@ func (as *AttributeService) Index(campID int, entID int, sync *time.Time) ([]*At
 		Data []*Attribute `json:"data"`
 	}
 
-	err = as.client.get(end, &wrap)
-	if err != nil {
+	if err = as.client.get(end, &wrap); err != nil {
 		return nil, fmt.Errorf("cannot get Attribute Index from Campaign (ID: %d): %w", campID, err)
 	}
 
 	return wrap.Data, nil
 }
 
-// Get returns the Attribute associated with atrID from the Campaign
-// associated with campID.
+// Get returns the Attribute associated with atrID for the entity associated
+// with entID from the Campaign associated with campID.
 func (as *AttributeService) Get(campID int, entID int, atrID int) (*Attribute, error) {
-	end, err := EndpointCampaign.ID(campID)
-	if err != nil {
+	var err error
+	end := EndpointCampaign
+
+	if end, err = end.ID(campID); err != nil {
 		return nil, fmt.Errorf("invalid Campaign ID: %w", err)
 	}
 	end = end.Concat(endpointEntity)
 
-	end, err = end.ID(entID)
-	if err != nil {
+	if end, err = end.ID(entID); err != nil {
 		return nil, fmt.Errorf("invalid Entity ID: %w", err)
 	}
 	end = end.Concat(as.end)
 
-	end, err = end.ID(atrID)
-	if err != nil {
+	if end, err = end.ID(atrID); err != nil {
 		return nil, fmt.Errorf("invalid Attribute ID: %w", err)
 	}
 
@@ -109,26 +110,26 @@ func (as *AttributeService) Get(campID int, entID int, atrID int) (*Attribute, e
 		Data *Attribute `json:"data"`
 	}
 
-	err = as.client.get(end, &wrap)
-	if err != nil {
+	if err = as.client.get(end, &wrap); err != nil {
 		return nil, fmt.Errorf("cannot get Attribute (ID: %d) from Campaign (ID: %d): %w", atrID, campID, err)
 	}
 
 	return wrap.Data, nil
 }
 
-// Create creates a new Attribute in the Campaign associated with campID using
-// the provided SimpleAttribute data.
+// Create creates a new Attribute for the entity associated with entID in the
+// Campaign associated with campID using the provided SimpleAttribute data.
 // Create returns the newly created Attribute.
 func (as *AttributeService) Create(campID int, entID int, atr SimpleAttribute) (*Attribute, error) {
-	end, err := EndpointCampaign.ID(campID)
-	if err != nil {
+	var err error
+	end := EndpointCampaign
+
+	if end, err = end.ID(campID); err != nil {
 		return nil, fmt.Errorf("invalid Campaign ID: %w", err)
 	}
 	end = end.Concat(endpointEntity)
 
-	end, err = end.ID(entID)
-	if err != nil {
+	if end, err = end.ID(entID); err != nil {
 		return nil, fmt.Errorf("invalid Entity ID: %w", err)
 	}
 	end = end.Concat(as.end)
@@ -142,32 +143,32 @@ func (as *AttributeService) Create(campID int, entID int, atr SimpleAttribute) (
 		Data *Attribute `json:"data"`
 	}
 
-	err = as.client.post(end, bytes.NewReader(b), &wrap)
-	if err != nil {
+	if err = as.client.post(end, bytes.NewReader(b), &wrap); err != nil {
 		return nil, fmt.Errorf("cannot create Attribute (Name: %s) for Campaign (ID: %d): %w", atr.Name, campID, err)
 	}
 
 	return wrap.Data, nil
 }
 
-// Update updates an existing Attribute associated with atrID from the
-// Campaign associated with campID using the provided SimpleAttribute data.
+// Update updates an existing Attribute associated with atrID for the entity
+// associated with entID from the Campaign associated with campID using the
+// provided SimpleAttribute data.
 // Update returns the newly updated Attribute.
 func (as *AttributeService) Update(campID int, entID int, atrID int, atr SimpleAttribute) (*Attribute, error) {
-	end, err := EndpointCampaign.ID(campID)
-	if err != nil {
+	var err error
+	end := EndpointCampaign
+
+	if end, err = end.ID(campID); err != nil {
 		return nil, fmt.Errorf("invalid Campaign ID: %w", err)
 	}
 	end = end.Concat(endpointEntity)
 
-	end, err = end.ID(entID)
-	if err != nil {
+	if end, err = end.ID(entID); err != nil {
 		return nil, fmt.Errorf("invalid Entity ID: %w", err)
 	}
 	end = end.Concat(as.end)
 
-	end, err = end.ID(atrID)
-	if err != nil {
+	if end, err = end.ID(atrID); err != nil {
 		return nil, fmt.Errorf("invalid Attribute ID: %w", err)
 	}
 
@@ -180,8 +181,7 @@ func (as *AttributeService) Update(campID int, entID int, atrID int, atr SimpleA
 		Data *Attribute `json:"data"`
 	}
 
-	err = as.client.put(end, bytes.NewReader(b), &wrap)
-	if err != nil {
+	if err = as.client.put(end, bytes.NewReader(b), &wrap); err != nil {
 		return nil, fmt.Errorf("cannot update Attribute (Name: %s) for Campaign (ID: %d): '%w'", atr.Name, campID, err)
 	}
 
@@ -191,25 +191,24 @@ func (as *AttributeService) Update(campID int, entID int, atrID int, atr SimpleA
 // Delete deletes an existing Attribute associated with atrID from the
 // Campaign associated with campID.
 func (as *AttributeService) Delete(campID int, entID int, atrID int) error {
-	end, err := EndpointCampaign.ID(campID)
-	if err != nil {
+	var err error
+	end := EndpointCampaign
+
+	if end, err = end.ID(campID); err != nil {
 		return fmt.Errorf("invalid Campaign ID: %w", err)
 	}
 	end = end.Concat(endpointEntity)
 
-	end, err = end.ID(entID)
-	if err != nil {
+	if end, err = end.ID(entID); err != nil {
 		return fmt.Errorf("invalid Entity ID: %w", err)
 	}
 	end = end.Concat(as.end)
 
-	end, err = end.ID(atrID)
-	if err != nil {
+	if end, err = end.ID(atrID); err != nil {
 		return fmt.Errorf("invalid Attribute ID: %w", err)
 	}
 
-	err = as.client.delete(end)
-	if err != nil {
+	if err = as.client.delete(end); err != nil {
 		return fmt.Errorf("cannot delete Attribute (ID: %d) for Campaign (ID: %d): %w", atrID, campID, err)
 	}
 
